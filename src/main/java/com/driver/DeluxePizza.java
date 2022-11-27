0df6f2721a09aa1120c3e3f651f8ed0a18c9970b
Bharat <aimhigh050@gmail.com>
2022-11-27 18:14:40 +0530

    First Attempt:

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,10 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        if(isVeg)
+            super.setBillInt(super.getBillInt()+ 150);
+        else
+            super.setBillInt(super.getBillInt()+ 200);
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,31 +4,60 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+//    private String bill;
+    private int billInt=0;
+
+    public void setBillInt(int billInt) {
+        this.billInt = billInt;
+    }
+
+    public int getBillInt() {
+        return billInt;
+    }
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+
         // your code goes here
+        if(isVeg)
+            this.price= 300;
+        else
+            this.price = 400;
+        billInt += price;
     }
 
     public int getPrice(){
         return this.price;
     }
-
+    private int flagC = 0;
     public void addExtraCheese(){
         // your code goes here
+        if(flagC==0){
+            billInt += 80;
+            flagC = 1;
+        }
     }
 
+    private int flagT = 0;
     public void addExtraToppings(){
         // your code goes here
+        if(flagT==0){
+            if(isVeg) billInt += 70;
+            else billInt += 120;
+            flagT=1;
+        }
     }
-
+    private int flagA = 0;
     public void addTakeaway(){
         // your code goes here
+        if(flagA ==0) {
+            billInt += 20;
+            flagA =1;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return Integer.toString(billInt);
     }
 }
